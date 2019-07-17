882d454a518f4a83c29fd778eb7d10ae7b3811ae
Deftsilence <1125247357@qq.com>
2019-07-16 20:28:26 -0700

    first commit

---
+++ version.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ap_version.h"
+
+#define THISFIRMWARE "cx07 V0.0.3-dev"
+#define FIRMWARE_VERSION 0,0,3,FIRMWARE_VERSION_TYPE_DEV
+
+#ifndef GIT_VERSION
+#define FIRMWARE_STRING THISFIRMWARE
+#else
+#define FIRMWARE_STRING THISFIRMWARE " (" GIT_VERSION ")"
+#endif
